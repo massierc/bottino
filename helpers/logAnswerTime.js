55c49427b8412e450ee4cedb0f3d2d5ec7eca3dd
massierc <massimo.ercolani28@gmail.com>
2019-03-13 16:09:52 +0100

    log user info on request

--- helpers/logAnswerTime.js
+++ helpers/logAnswerTime.js
@@ -1,6 +1,11 @@
-module.exports = function logAnswerTime(ctx, name) {
-  console.info(
-    `${name} answered in ${(new Date().getTime() - ctx.timeReceived.getTime()) /
-      1000}s`
-  )
+function logAnswerTime(ctx, name) {
+  const now = new Date()
+  const options = { day: 'numeric', year: 'numeric', month: 'numeric', hour: 'numeric', minute: 'numeric' }
+  const timestamp = now.toLocaleDateString('it', options)
+  const user = ctx.message.from
+  console.info(`[${timestamp}] - ${user.first_name} ${user.last_name} (${user.username})`)
+  console.info(`${name} answered in ${(now.getTime() - ctx.timeReceived.getTime()) / 1000}s`)
+  console.info('')
 }
+
+module.exports = logAnswerTime
