22fe7586912d5801db03184a19fe82e65a10a245
massierc <massimo.ercolani28@gmail.com>
2019-03-13 12:55:39 +0100

    clean up error message (hide key and token)

--- helpers/trello.js
+++ helpers/trello.js
@@ -1,13 +1,24 @@
 const fetch = require('node-fetch')
 const { trelloUrl } = require('./url')
+const qs = require('querystring')
 
-async function createCard(listId = '5c8798ee34ac6b81df7366dd', payload) {
-  const params = {
-    listId,
-    payload,
+function safeError(err) {
+  const regexp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/ig
+  const url = new URL(err.message.match(regexp)[0])
+  const path = url.origin + url.pathname
+  const { key, token, ...cleanQuery } = qs.parse(url.search.slice(1))
+  const cleanUrl = `${path}?${qs.stringify(cleanQuery)}`
+  err.message = err.message.replace(regexp, cleanUrl)
+  return err
+}
+
+async function createCard(params) {
+  try {
+    const response = await fetch(trelloUrl(params), { method: 'POST' })
+    return await response.json()
+  } catch (err) {
+    throw safeError(err)
   }
-  const response = await fetch(trelloUrl(params))
-  return await response.json()
 }
 
 module.exports = createCard
