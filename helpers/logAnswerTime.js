420b1995c0a2361bbb5d36563ece062f59d4e07d
massierc <massimo.ercolani28@gmail.com>
2019-03-13 16:39:45 +0100

    isolate timestampAndUser

--- helpers/logAnswerTime.js
+++ helpers/logAnswerTime.js
@@ -1,11 +1,14 @@
-function logAnswerTime(ctx, name) {
-  const now = new Date()
+function timestampAndUser(ctx) {
   const options = { day: 'numeric', year: 'numeric', month: 'numeric', hour: 'numeric', minute: 'numeric' }
-  const timestamp = now.toLocaleDateString('it', options)
+  const timestamp = new Date().toLocaleDateString('it', options)
   const user = ctx.message.from
-  console.info(`[${timestamp}] - ${user.first_name} ${user.last_name} (${user.username})`)
-  console.info(`${name} answered in ${(now.getTime() - ctx.timeReceived.getTime()) / 1000}s`)
-  console.info('')
+  const name = user.first_name && `${user.first_name} ${user.last_name}`
+  const username = user.username && ` (@${user.username})`
+  return `[${timestamp}] ${name + username}`
+}
+
+function logAnswerTime(ctx, name) {
+  console.info(`${timestampAndUser(ctx)}: ${name} answered in ${(new Date().getTime() - ctx.timeReceived.getTime()) / 1000}s`)
 }
 
 module.exports = logAnswerTime
