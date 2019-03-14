0b951ec3a17492327d6bd33be8feff1a535d1dfc
massierc <massimo.ercolani28@gmail.com>
2019-03-14 19:21:10 +0100

    fix

--- helpers/logAnswerTime.js
+++ helpers/logAnswerTime.js
@@ -1,14 +1,16 @@
 function timestampAndUser(user) {
   const options = { day: 'numeric', year: 'numeric', month: 'numeric', hour: 'numeric', minute: 'numeric' }
   const timestamp = new Date().toLocaleDateString('it', options)
+  if (!user) return `[${timestamp}] unknown user: `
   const name = user.first_name && `${user.first_name} ${user.last_name}`
   const username = user.username && ` (@${user.username})`
   return `[${timestamp}] ${name + username}: `
 }
 
 function logAnswerTime(ctx, name) {
+  const user = ctx.update.message && ctx.update.message.from
   console.info(
-    timestampAndUser(ctx.update.message.from),
+    timestampAndUser(user),
     `${name} answered in ${(new Date().getTime() - ctx.timeReceived.getTime()) / 1000}s`
   )
 }
